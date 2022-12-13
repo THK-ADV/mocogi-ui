ae116d619711f9b42c1158089ed6121647e801b9
Alexander Dobrynin <info@alex-dobrynin.com>
2022-12-13 09:58:08 +0100

    mock data

--- src/app/http/http.service.ts
+++ src/app/http/http.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
-import { map, Observable } from 'rxjs'
+import { map, Observable, of } from 'rxjs'
 import { environment } from '../../environments/environment'
 
 export interface Metadata {
@@ -22,5 +22,9 @@ export class HttpService {
     this.http.get<Metadata[]>(`${environment.backendUrl}/metadata`)
 
   medataDataForUser = (user: String): Observable<Metadata[]> =>
-    this.http.get<Metadata[]>(`${environment.backendUrl}/metadata?user=${user}`)
+    of([
+      {title: 'Algorithmen und Programmierung 2', id: '1', abbrev: 'ap2'},
+      {title: 'Paradigmen der Programmierung 2', id: '2', abbrev: 'pp'},
+    ])
+  //this.http.get<Metadata[]>(`${environment.backendUrl}/metadata?user=${user}`)
 }
