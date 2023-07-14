421d8c3c566c57484aca55962539c49171ce672f
Yannic Brügger <yannic@bruegger.dev>
2023-07-14 19:36:50 +0200

    feat: add nav-bar component

---
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+export type CopsNavigationLink = {
+  type: "LINK",
+  text: string
+  icon: string,
+  details?: string,
+  url: string
+};
+
+export type CopsNavigationHeader = {
+  type: "HEADER",
+  text: string,
+};
+
+export type CopsNavigationItem = CopsNavigationHeader | CopsNavigationLink;
+
+@Component({
+  selector: 'cops-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent {
+  @Input() navItems: Array<CopsNavigationItem> = [];
+}
